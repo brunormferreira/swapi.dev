b9e581689511127b8fdd09109c03062415cc32a3
Bruno <brunormferreira@hotmail.com>
2020-10-21 14:34:45 -0300

    add(feat): use with mock

--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 
 import Brand from '../components/Brand/Brand.component';
 import Footer from '../components/Footer/Footer.component';
-
+import Movies from '../components/Movie/Movies.component';
 import { ContainerApp } from './Main.styles';
 
 const Main: React.FC = () => {
+  const movies = {
+    title: 'Teste title',
+    episode_id: '2',
+    release_date: '140596',
+    director: 'ABDF',
+    producer: 'Bdadaf',
+  };
+
   return (
     <ContainerApp>
       <Brand />
-      <div>
-        <p>Hello from main page</p>
-        <p>Hello from main page</p>
-        <p>Hello from main page</p>
-        <p>Hello from main page</p>
-      </div>
+      <Movies movie={movies} />
+      <Movies movie={movies} />
+      <Movies movie={movies} />
+      <Movies movie={movies} />
+
       <Footer />
     </ContainerApp>
   );
