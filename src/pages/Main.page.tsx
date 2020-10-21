1e5ae35ead558622d5a7f899833d272dd7e5170a
Bruno <brunormferreira@hotmail.com>
2020-10-21 15:37:56 -0300

    add(feat): remove mock and add real data

--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
 
 import Brand from '../components/Brand/Brand.component';
+import AppName from '../components/Title/Title.component';
+import MovieList from '../components/MovieList/MovieList.component';
 import Footer from '../components/Footer/Footer.component';
-import Movies from '../components/Movie/Movies.component';
+
 import { ContainerApp } from './Main.styles';
 
-const Main: React.FC = () => {
-  const movies = {
-    title: 'Teste title',
-    episode_id: '2',
-    release_date: '140596',
-    director: 'ABDF',
-    producer: 'Bdadaf',
-  };
-
-  return (
-    <ContainerApp>
-      <Brand />
-      <Movies movie={movies} />
-      <Movies movie={movies} />
-      <Movies movie={movies} />
-      <Movies movie={movies} />
-
-      <Footer />
-    </ContainerApp>
-  );
-};
+const Main: React.FC = (): JSX.Element => (
+  <ContainerApp>
+    <Brand />
+    <AppName />
+    <MovieList />
+    <Footer />
+  </ContainerApp>
+);
 
 export default Main;
