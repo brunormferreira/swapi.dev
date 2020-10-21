6efe299a81bf555ec5c37b0dfd919eae017a4430
Bruno <brunormferreira@hotmail.com>
2020-10-21 10:18:23 -0300

    add(feat): use global styles from styled

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 import GlobalStyles from './assets/styles/globals';
 
-const App = () => {
-  return <GlobalStyles />;
+import Main from './pages/Main';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main: React.FC = () => {
+  return <p>Hello from main page</p>;
+};
+
+export default Main;
