baac8e3968fd6483d278ae71167a9b2da2fcf38d
Bruno <brunormferreira@hotmail.com>
2020-10-22 10:48:44 -0300

    add(feat): modal component

--- src/components/Modal/Modal.component.tsx
+++ src/components/Modal/Modal.component.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import {} from './Modal.styles';
+import ReducerEnum from '../../context/modal/enum';
+
+import { useModal } from '../../context/modal';
+
+import useOutsideClick from '../../utils/useOutsideClick';
+
+import {
+  ContainerBackground,
+  ModalFullScreen,
+  ModalContainer,
+  TitleModal,
+  ModalBody,
+} from './Modal.styles';
 
 const Modal: React.FC = () => {
-  return <p>Oi</p>;
+  const { dispatch } = useModal();
+
+  const dialogContainer = useRef() as React.MutableRefObject<
+    HTMLTableRowElement
+  >;
+
+  const onClose = (): void => dispatch({ type: ReducerEnum.SET_INVISIBLE });
+
+  useOutsideClick(dialogContainer, () => {
+    onClose();
+  });
+
+  return (
+    <>
+      <ContainerBackground />
+      <ModalFullScreen>
+        <ModalContainer ref={dialogContainer}>
+          <button type="button" onClick={onClose}>
+            X
+          </button>
+          <ModalBody>
+            <TitleModal>TESTE</TitleModal>
+          </ModalBody>
+        </ModalContainer>
+      </ModalFullScreen>
+    </>
+  );
 };
 
 export default Modal;
--- src/components/Modal/Modal.styles.tsx
+++ src/components/Modal/Modal.styles.tsx
@@ -1,1 +1,48 @@
 import styled from 'styled-components';
+
+export const ContainerBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #3d3d3d 0% 0% no-repeat padding-box;
+  opacity: 0.3;
+  z-index: 99;
+`;
+
+export const ModalFullScreen = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  width: 472px;
+  min-height: 200px;
+  background: #fff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 2px 8px #0000001a;
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+export const ModalBody = styled.div`
+  justify-content: center;
+  padding: 0 64px;
+`;
+
+export const TitleModal = styled.h1`
+  text-align: center;
+  font-weight: 400;
+  font-size: 28px;
+  letter-spacing: 0;
+  margin: 0 0 16px;
+  color: black;
+`;
