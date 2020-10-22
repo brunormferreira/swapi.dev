b003d118fc8af90a4eb68c1eaab8ef9e03c61cfd
Bruno <brunormferreira@hotmail.com>
2020-10-22 10:49:16 -0300

    add(feat): utils to modal

---
+++ src/utils/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+const useOutsideClick = (ref: any, callback: any) => {
+  const handleClick = (e: any) => {
+    if (!ref.current || ref.current.contains(e.target)) {
+      return;
+    }
+
+    callback();
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+};
+
+export default useOutsideClick;
