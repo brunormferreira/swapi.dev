27c2d0fc4892786818dfe2dcdbfe83293f941ec6
Bruno <brunormferreira@hotmail.com>
2020-10-22 18:36:59 -0300

    add(feat): variable colors

--- src/assets/styles/variables.ts
+++ src/assets/styles/variables.ts
@@ -1,11 +1,17 @@
 interface ColorsInterface {
   yellow: string;
+  lightYellow: string;
   black: string;
   white: string;
+  gray: string;
+  overlay: string;
 }
 
 export const colors: ColorsInterface = {
   yellow: '#FFE81F',
+  lightYellow: '#F3F057',
   black: '#000000',
+  gray: '#4B4B4B',
   white: '#FFF',
+  overlay: 'rgba(0, 0, 0, 0.5)',
 };
