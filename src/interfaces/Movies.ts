430d4aa6c2e74f8434e9503e6e918bdc47ece645
Bruno <brunormferreira@hotmail.com>
2020-10-23 09:43:02 -0300

    fix(model): movie interface

---
+++ src/interfaces/Movies.ts
@@ -0,0 +1,10 @@
+export default interface MoviesInterface {
+  characters?: string[];
+  created?: string;
+  director: string;
+  episode_id: number;
+  opening_crawl: string;
+  producer: string;
+  release_date: string;
+  title: string;
+}
